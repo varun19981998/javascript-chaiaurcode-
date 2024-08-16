a71e4caa5af6feaaea8d2857d7bbb02f2d90cf81
varun19981998 <varunpsingh9898@gmail.com>
2024-08-16 20:53:25 +0530

    origin

---
+++ colorchanging/index.js
@@ -0,0 +1,25 @@
+ const body=document.querySelector('body');
+ const  buttons=document.querySelectorAll('.button')
+ buttons.forEach(function(button){
+  console.log(button);
+   button.addEventListener('click' ,function(e){
+    console.log(e);
+    console.log(e.target);
+    if(e.target.id==='grey'){
+      // body.style.backgroundColor='grey';
+      body.style.backgroundColor=e.target.id;
+    }
+    if(e.target.id==='white'){
+      body.style.backgroundColor='white';
+    }
+    if(e.target.id==='blue'){
+      body.style.backgroundColor='blue';
+    }
+    if(e.target.id==='yellow'){
+      body.style.backgroundColor='yellow';
+    }
+
+
+
+   })
+ })
